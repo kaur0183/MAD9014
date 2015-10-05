d8001b4a97c677212a7849a044b9d3a8ee24fa5f
Prabhjot kaur <kaue0183@algonquinlive.com>
2015-10-05 18:45:01 -0400

    this is my  assignment

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,17 @@
-// JavaScript Document
-
+var bigRectangle = {};
+var smallRectangle = {};
+window.onload = function () {
+	bigRectangle = document.querySelector(".long-rect");
+	smallRectangle = document.querySelector(".short-rect");
+	console.log("Welcome...");
+	bigRectangle.addEventListener("mouseover", function () {
+		smallRectangle.innerText = "Mouse is on the box";
+		smallRectangle.addEventListener("click", function () {
+			alert("Hello")
+		})
+	});
+	bigRectangle.addEventListener("mouseout", function () {
+		smallRectangle.innerText = "Mouse is not on the box"
+	})
+};
 
